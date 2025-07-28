03550c4c151f0ce9def95a4819bd398fe51684ee
yizhang2077 <1109276519@qq.com>
2025-07-28 15:39:23 +0800

    rename config

--- examples/run_qwen3_coder_eagle3_offline.sh
+++ examples/run_qwen3_coder_eagle3_offline.sh
@@ -9,14 +9,13 @@ torchrun \
     --nproc_per_node $NUM_GPUS \
     $ROOT_DIR/scripts/train_eagle3_offline.py \
     --target-model-path Qwen/Qwen3-Coder-480B-A35B-Instruct \
-    --draft-model-config $ROOT_DIR/configs/qwen3-coder-eagle3.json \
+    --draft-model-config $ROOT_DIR/configs/qwen3-coder-480B-A35B-instruct-eagle3.json \
     --train-data-path $ROOT_DIR/cache/dataset/opc.jsonl \
     --train-hidden-states-path $ROOT_DIR/cache/hidden_states \
-    --output-dir $ROOT_DIR/outputs-400k/Qwen3-Coder-480B-A35B-Instruct \
+    --output-dir $ROOT_DIR/outputs/Qwen3-Coder-480B-A35B-Instruct \
     --num-epochs 10 \
     --batch-size 1 \
     --learning-rate 1e-4 \
     --max-length 2048 \
     --chat-template qwen \
     --resume
-
