6de4b4658bad76e0fc2b5e75a18256c410653862
yd-oom <wydyszd3613@gmail.com>
2025-08-19 00:00:31 +0800

    fix: remove swanlab isrunning check

--- specforge/tracker.py
+++ specforge/tracker.py
@@ -196,7 +196,7 @@ class SwanlabTracker(Tracker):
             swanlab.log(log_dict, step=step)
 
     def close(self):
-        if self.rank == 0 and self.is_initialized and swanlab.is_running():
+        if self.rank == 0 and self.is_initialized:
             swanlab.finish()
             self.is_initialized = False
 
