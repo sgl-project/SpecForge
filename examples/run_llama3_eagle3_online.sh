c5d31b1bc5db407db08ff9365a035e0101dddaab
Baoyuan Qi <qibaoyuan@126.com>
2025-08-18 20:54:54 +0800

    rollback

--- examples/run_llama3_eagle3_online.sh
+++ examples/run_llama3_eagle3_online.sh
@@ -1,33 +1,22 @@
-export PYTHONPATH=/Users/qibaoyuan/PycharmProjects/SpecForge-own:${PYTHONPATH}
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 ROOT_DIR=$(dirname $SCRIPT_DIR)
 
 # train eagle3 for llama3.1-8b
 NUM_GPUS=${1:-8}
-NUM_GPUS=1
 
 torchrun \
     --standalone \
     --nproc_per_node $NUM_GPUS \
     $ROOT_DIR/scripts/train_eagle3_online.py \
-    --target-model-path /Users/qibaoyuan/Documents/llm/Meta-Llama-3.1-8B-Instruct  \
-    --draft-model-config $ROOT_DIR/configs/llama3-8B-eagle3.json \
-    --train-data-path /Users/qibaoyuan/PycharmProjects/SpecForge-own/examples/sharegpt.jsonl \
-    --output-dir $ROOT_DIR/outputs/llama3-8b-eagle3 \
-    --num-epochs 5 \
+    --target-model-path meta-llama/Llama-4-Scout-17B-16E \
+    --draft-model-config $ROOT_DIR/configs/llama4-scout-17B-16E-eagle3.json \
+    --train-data-path $ROOT_DIR/cache/dataset/sharegpt.jsonl \
+    --output-dir $ROOT_DIR/outputs/llama4-scout-17B-16E-eagle3 \
+    --num-epochs 10 \
     --batch-size 1 \
     --learning-rate 1e-4 \
     --max-length 2048 \
-    --chat-template llama3 \
-    --cache-dir $ROOT_DIR/cache
-class LossKwargs(TypedDict, total=False):
-    """
-    Keyword arguments to be passed to the loss function
-
-    Attributes:
-        num_items_in_batch (`int`, *optional*):
-            Number of items in the batch. It is recommended to pass it when
-            you are doing gradient accumulation.
-    """
-
-    num_items_in_batch: Optional[int]
+    --chat-template llama4 \
+    --cache-dir $ROOT_DIR/cache \
+    --embedding-key language_model.model.embed_tokens.weight \
+    --tp-size $NUM_GPUS
