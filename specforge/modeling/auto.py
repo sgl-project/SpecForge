1ef159aad150589679a7c6d0c4e4faf6b79fe96d
fzyzcjy <ch271828n@outlook.com>
2025-08-19 21:47:55 +0800

    Revert "fmt"
    This reverts commit 4e81f9433448457b17efbeffa68cf7aa0b1af77a.

--- specforge/modeling/auto.py
+++ specforge/modeling/auto.py
@@ -19,7 +19,6 @@ from transformers import (
 from specforge.utils import default_torch_dtype
 
 from .draft.llama3_eagle import LlamaForCausalLMEagle3
-
 # from .target.llama import LlamaForCausalLM
 from .target.llama4 import Llama4ForCausalLM
 from .target.qwen3 import Qwen3ForCausalLM
