23a0c7336d23845ea21d4e68136100c39b886f18
FrankLeeeee <somerlee.9@gmail.com>
2025-09-01 02:40:21 -0700

    polish

--- scripts/train_eagle3_online.py
+++ scripts/train_eagle3_online.py
@@ -14,7 +14,6 @@ from torch.distributed.fsdp import MixedPrecision, ShardingStrategy, StateDictTy
 from tqdm import tqdm
 from transformers import AutoConfig, AutoModelForCausalLM, AutoTokenizer
 
-import wandb
 from specforge import (
     AutoDistributedTargetModel,
     AutoDraftModelConfig,
