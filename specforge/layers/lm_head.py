3d8f27e55024f3797e8f3955a1bbf5541f7ea427
FrankLeeeee <somerlee.9@gmail.com>
2025-11-09 17:18:01 +0000

    polihs

--- specforge/layers/lm_head.py
+++ specforge/layers/lm_head.py
@@ -10,20 +10,6 @@ from specforge.distributed import get_tp_group, shard_tensor
 
 
 class ParallelLMHead(nn.Module):
-    """Parallelized LM head.
-
-    Output logits weight matrices used in the Sampler. The weight and bias
-    tensors are padded to make sure they are divisible by the number of
-    model parallel GPUs.
-
-    Args:
-        num_embeddings: vocabulary size.
-        embedding_dim: size of hidden state.
-        bias: whether to use bias.
-        params_dtype: type of the parameters.
-        org_num_embeddings: original vocabulary size (without LoRA).
-        padding_size: padding size for the vocabulary.
-    """
 
     def __init__(
         self,
