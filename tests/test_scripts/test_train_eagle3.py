f5d3d1df29c1e851b59d9b9c7351898f8e9cd5f9
FrankLeeeee <somerlee.9@gmail.com>
2025-11-27 05:57:03 +0000

    polish

--- tests/test_scripts/test_train_eagle3.py
+++ tests/test_scripts/test_train_eagle3.py
@@ -1,3 +1,4 @@
+import shutil
 import unittest
 from pathlib import Path
 
@@ -50,47 +51,86 @@ class TestTrainEagle3(unittest.TestCase):
             for line in script:
                 f.write(line)
 
-    def test_online_train_eagle3_with_sglang_backend(self):
-        # run training
-        train_process = execute_shell_command(
-            "bash examples/run_llama3.1_8b_eagle3_online.sh 2"
-        )
-        train_process.wait()
-        self.assertEqual(train_process.returncode, 0)
+    # def test_online_train_eagle3_with_sglang_backend(self):
+    #     # run training
+    #     train_process = execute_shell_command(
+    #         "bash examples/run_llama3.1_8b_eagle3_online.sh 2"
+    #     )
+    #     train_process.wait()
+    #     self.assertEqual(train_process.returncode, 0)
 
-    def test_online_train_eagle3_with_hf_backend(self):
-        # replace --target-model-backend sglang with --target-model-backend hf
+    # def test_online_train_eagle3_with_hf_backend(self):
+    #     # replace --target-model-backend sglang with --target-model-backend hf
+    #     script_path = Path(__file__).parent.parent.parent.joinpath(
+    #         "examples", "run_llama3.1_8b_eagle3_online.sh"
+    #     )
+    #     replace_in_script(
+    #         script_path, "--target-model-backend sglang", "--target-model-backend hf"
+    #     )
+
+    #     # run training
+    #     train_process = execute_shell_command(
+    #         "bash examples/run_llama3.1_8b_eagle3_online.sh 2"
+    #     )
+    #     train_process.wait()
+    #     self.assertEqual(train_process.returncode, 0)
+
+    # def test_online_train_eagle3_with_custom_backend(self):
+    #     # replace --target-model-backend sglang with --target-model-backend custom
+    #     script_path = Path(__file__).parent.parent.parent.joinpath(
+    #         "examples", "run_llama3.1_8b_eagle3_online.sh"
+    #     )
+    #     replace_in_script(
+    #         script_path,
+    #         "--target-model-backend sglang",
+    #         "--target-model-backend custom",
+    #     )
+
+    #     # run training
+    #     train_process = execute_shell_command(
+    #         "bash examples/run_llama3.1_8b_eagle3_online.sh 2"
+    #     )
+    #     train_process.wait()
+    #     self.assertEqual(train_process.returncode, 0)
+
+    def test_offline_train_eagle3(self):
+        # remove the hidden states if they exist
         script_path = Path(__file__).parent.parent.parent.joinpath(
-            "examples", "run_llama3.1_8b_eagle3_online.sh"
-        )
-        replace_in_script(
-            script_path, "--target-model-backend sglang", "--target-model-backend hf"
-        )
-
-        # run training
-        train_process = execute_shell_command(
-            "bash examples/run_llama3.1_8b_eagle3_online.sh 2"
-        )
-        train_process.wait()
-        self.assertEqual(train_process.returncode, 0)
-
-    def test_online_train_eagle3_with_custom_backend(self):
-        # replace --target-model-backend sglang with --target-model-backend custom
-        script_path = Path(__file__).parent.parent.parent.joinpath(
-            "examples", "run_llama3.1_8b_eagle3_online.sh"
+            "examples", "run_llama3.1_8b_eagle3_offline.sh"
         )
         replace_in_script(
             script_path,
-            "--target-model-backend sglang",
-            "--target-model-backend custom",
+            "meta-llama/Llama-3.1-8B-Instruct",
+            "nreHieW/Llama-3.1-8B-Instruct",
+        )
+        replace_in_script(
+            script_path,
+            "--batch-size 32",
+            "--batch-size 5",
+        )
+        replace_in_script(
+            script_path,
+            "scripts/prepare_hidden_states.py",
+            "scripts/prepare_hidden_states.py --num-samples 10",
+        )
+        replace_in_script(
+            script_path,
+            "$ROOT_DIR/scripts/train_eagle3.py",
+            "$ROOT_DIR/scripts/train_eagle3.py --max-num-steps 2",
         )
 
-        # run training
-        train_process = execute_shell_command(
-            "bash examples/run_llama3.1_8b_eagle3_online.sh 2"
+        hidden_states_path = Path(__file__).parent.parent.parent.joinpath(
+            "cache", "hidden_states", "sharegpt_train_Llama-3.1-8B-Instruct"
         )
-        train_process.wait()
-        self.assertEqual(train_process.returncode, 0)
+        if hidden_states_path.exists():
+            # delete the directory
+            shutil.rmtree(hidden_states_path)
+
+        training_process = execute_shell_command(
+            "bash examples/run_llama3.1_8b_eagle3_offline.sh 2",
+        )
+        training_process.wait()
+        self.assertEqual(training_process.returncode, 0)
 
 
 if __name__ == "__main__":
