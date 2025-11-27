be573d49ab7f58bff85c628975493411d7cef24d
FrankLeeeee <somerlee.9@gmail.com>
2025-11-27 04:15:07 +0000

    polish

--- examples/run_llama3.1_8b_eagle3_online.sh
+++ examples/run_llama3.1_8b_eagle3_online.sh
@@ -1,16 +1,14 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 ROOT_DIR=$(dirname $SCRIPT_DIR)
 export TORCHINDUCTOR_CACHE_DIR=$ROOT_DIR/cache/compiled_kernels
-
 # train eagle3 for llama3.1-8b
 NUM_GPUS=${1:-1}
 TP_SIZE=${2:-1}
-
 torchrun \
     --standalone \
     --nproc_per_node $NUM_GPUS \
     $ROOT_DIR/scripts/train_eagle3.py \
-    --target-model-path meta-llama/Llama-3.1-8B-Instruct \
+    --target-model-path nreHieW/Llama-3.1-8B-Instruct \
     --draft-model-config $ROOT_DIR/configs/llama3-8B-eagle3.json \
     --train-data-path $ROOT_DIR/cache/dataset/sharegpt_train.jsonl \
     --output-dir $ROOT_DIR/outputs/llama3-8b-eagle3-sharegpt \
@@ -22,5 +20,5 @@ torchrun \
     --chat-template llama3 \
     --cache-dir $ROOT_DIR/cache \
     --attention-backend sdpa \
-    --target-model-backend sglang \
-    --log-interval 10
+    --target-model-backend custom \
+    --log-interval 10 --max-num-steps 10 --max-num-steps 10 --max-num-steps 10 --max-num-steps 10 --max-num-steps 10
--- tests/test_scripts/test_train_eagle3.py
+++ tests/test_scripts/test_train_eagle3.py
@@ -1,14 +1,23 @@
 import unittest
 from pathlib import Path
 
-from sglang.utils import execute_shell_command
+from tests.utils import execute_shell_command
 
 CACHE_DIR = Path(__file__).parent.parent.parent.joinpath("cache")
 
 
+def replace_in_script(script_path: Path, pattern: str, replacement: str):
+    with open(script_path, "r") as f:
+        script = f.readlines()
+    script = [line.replace(pattern, replacement) for line in script]
+    with open(script_path, "w") as f:
+        for line in script:
+            f.write(line)
+
+
 class TestTrainEagle3(unittest.TestCase):
 
-    def test_online_train_eagle3(self):
+    def setUp(self) -> None:
         # prepare data
         data_process = execute_shell_command(
             "python scripts/prepare_data.py --dataset sharegpt"
@@ -28,6 +37,7 @@ class TestTrainEagle3(unittest.TestCase):
         script[-1] = script[-1].rstrip() + " --max-num-steps 10"
 
         # replace meta-llama/Llama-3.1-8B-Instruct with unsloth/Llama-3.2-1B-Instruct
+        # so that we don't need HF token for gated repo
         script = [
             line.replace(
                 "meta-llama/Llama-3.1-8B-Instruct", "nreHieW/Llama-3.1-8B-Instruct"
@@ -39,6 +49,41 @@ class TestTrainEagle3(unittest.TestCase):
         with open(script_path, "w") as f:
             for line in script:
                 f.write(line)
+
+    def test_online_train_eagle3_with_sglang_backend(self):
+        # run training
+        train_process = execute_shell_command(
+            "bash examples/run_llama3.1_8b_eagle3_online.sh 2"
+        )
+        train_process.wait()
+        self.assertEqual(train_process.returncode, 0)
+
+    def test_online_train_eagle3_with_hf_backend(self):
+        # replace --target-model-backend sglang with --target-model-backend hf
+        script_path = Path(__file__).parent.parent.parent.joinpath(
+            "examples", "run_llama3.1_8b_eagle3_online.sh"
+        )
+        replace_in_script(
+            script_path, "--target-model-backend sglang", "--target-model-backend hf"
+        )
+
+        # run training
+        train_process = execute_shell_command(
+            "bash examples/run_llama3.1_8b_eagle3_online.sh 2"
+        )
+        train_process.wait()
+        self.assertEqual(train_process.returncode, 0)
+
+    def test_online_train_eagle3_with_custom_backend(self):
+        # replace --target-model-backend sglang with --target-model-backend custom
+        script_path = Path(__file__).parent.parent.parent.joinpath(
+            "examples", "run_llama3.1_8b_eagle3_online.sh"
+        )
+        replace_in_script(
+            script_path,
+            "--target-model-backend sglang",
+            "--target-model-backend custom",
+        )
 
         # run training
         train_process = execute_shell_command(
