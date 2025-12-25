56606359751e066beada329ca2895fc53ffee0df
Shenggui Li <somerlee.9@gmail.com>
2025-12-25 16:32:10 +0800

    fixed templates (#389)
    * fixed templates
    
    * polish

--- specforge/data/template.py
+++ specforge/data/template.py
@@ -253,16 +253,15 @@ TEMPLATE_REGISTRY.register(
         user_header="<|im_start|>user\n",
         system_prompt="You are a helpful assistant.",
         end_of_turn_token="<|im_end|>",
+    ),
+)
 
 TEMPLATE_REGISTRY.register(
     name="deepseek-v3",
     template=ChatTemplate(
         assistant_header="<｜Assistant｜>",
         user_header="<｜User｜>",
-        system_prompt="",
-        end_of_turn_token="",
-        bos_token="<｜begin of sentence｜>",
-        eos_token="<｜end of sentence｜>",
-        parser_type="general",
+        system_prompt="You are a helpful assistant.",
+        end_of_turn_token="<｜end▁of▁sentence｜>",
     ),
 )
