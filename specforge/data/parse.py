159753f2a14a4a8be165a662bd91347bde845139
chenyefei.cyf <chenyefei.cyf@U-9V5T77LW-2356.local>
2025-12-29 21:31:45 +0800

    update parser and reference data

--- specforge/data/parse.py
+++ specforge/data/parse.py
@@ -86,14 +86,7 @@ class GeneralParser(Parser):
         if not self.tokenizer.pad_token_id:
             self.tokenizer.pad_token_id = self.tokenizer.unk_token_id
 
-        sep = re.escape(self.assistant_message_separator)
-        eos = re.escape(self.chat_template.end_of_turn_token)
-
-        if "<role>" in self.assistant_message_separator:
-            assistant_pattern = f"{sep}(.*?(?={eos}|<role|$))"
-        else:
-            assistant_pattern = f"{sep}(.*?(?:{eos}|$))"
-
+        assistant_pattern = f"{re.escape(self.assistant_message_separator)}(.*?(?:{re.escape(self.chat_template.end_of_turn_token)}|$))"
         try:
             # use fast tokenizer's offset mapping to create loss mask
             encoding = self.tokenizer(
