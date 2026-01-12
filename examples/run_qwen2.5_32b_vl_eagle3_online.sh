3da57e332b1eeef549be6a3298a671ba0a31ee04
gerayking <919179287@qq.com>
2026-01-12 20:24:21 +0800

    fix info

--- examples/run_qwen2.5_32b_vl_eagle3_online.sh
+++ examples/run_qwen2.5_32b_vl_eagle3_online.sh
@@ -13,9 +13,9 @@ torchrun \
     $ROOT_DIR/scripts/train_eagle3.py \
     --target-model-path /home/qspace/Qwen2.5-VL-32B-Instruct/ \
     --draft-model-config $ROOT_DIR/configs/qwen2.5-vl-32b-eagle3.json \
-    --train-data-path /mnt/cephfs/user_xuanweifu/data/datasets/xcx_eagle3_1230_perf.jsonl \
+    --train-data-path $ROOT_DIR/cache/dataset/allava4v_train.jsonl \
     --build-dataset-num-proc $BUILD_DATASET_NUM_PROC \
-    --output-dir /home/qspace/cache/Qwen2.5-VL-32B-Instruct \
+    --output-dir $ROOT_DIR/outputs/Qwen2.5-VL-32B-Instruct \
     --num-epochs 10 \
     --batch-size 1 \
     --learning-rate 1e-4 \
@@ -23,7 +23,7 @@ torchrun \
     --dist-timeout 360 \
     --chat-template qwen2-vl \
     --target-model-backend sglang \
-    --cache-dir /hom/qspace/cache \
+    --cache-dir $ROOT_DIR/cache \
     --embedding-key model.embed_tokens.weight \
     --tp-size 8 \
     --sglang-mem-fraction-static 0.5 \
